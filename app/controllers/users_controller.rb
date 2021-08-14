37d0aa935c1fbbc4f2d3ad6e5c2272960a348b65
Chaldrak <chaldrakus@gmail.com>
2021-08-14 16:46:13 +0100

    login and register ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
-  end
+    skip_before_action :login_required, only: [:new, :create]
+
+    def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to action: "show", id: @user.id
+            flash[:success] = "Compte créé avec succès. Veuillez vous connecter"
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :surname, :sexe, :email, :password, :password_confirmation)
+    end
 end
